66a84539ceb64d360370b77bde290531f4dfa13b
Tyler McGee <tylermcgee@tylers-macbook-air.local>
2017-05-25 15:38:33 -0400

    Initial Commit

---
+++ Finding The User Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Finding The User Location
+//
+//  Created by Tyler McGee on 5/25/17.
+//  Copyright © 2017 Tyler McGee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Finding The User LocationTests/Finding_The_User_LocationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Finding_The_User_LocationTests.swift
+//  Finding The User LocationTests
+//
+//  Created by Tyler McGee on 5/25/17.
+//  Copyright © 2017 Tyler McGee. All rights reserved.
+//
+
+import XCTest
+@testable import Finding_The_User_Location
+
+class Finding_The_User_LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
